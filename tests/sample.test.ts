bda73d10e4bb62566744f3c93f126f93031a3ad1
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-08-02 15:01:59 +0700

    belajar vitest

---
+++ tests/sample.test.ts
@@ -0,0 +1,16 @@
+import {describe, expect, it} from "vitest";
+
+describe('sample', () => {
+    it('should pass', () => {
+        expect(1).toBe(1);
+    });
+    it('should pass 2', () => {
+        expect(2).toBe(2);
+    });
+    it('should pass 3', () => {
+        expect(3).toBe(3);
+    });
+    it('should pass 4', () => {
+        expect(4).toBe(4);
+    });
+});
